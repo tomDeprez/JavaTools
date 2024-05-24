35402bbbcc78ee49781615f63cb885a2277d4601
Tom DEPREZ <tom_deprez@hotmail.fr>
2024-05-24 16:17:18 +0200

    XSS

---
+++ src/main/java/com/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, 
+                        HttpServletResponse response, HttpServletRequest request) {
+        // Pour des raisons de sécurité, vous devriez vérifier le nom d'utilisateur et
+        // le mot de passe dans une base de données
+        // Ici, nous allons simplement simuler une connexion réussie pour tout
+        // utilisateur
+        if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
+            // Simuler une connexion réussie en créant un cookie
+            Cookie userCookie = new Cookie("user", "HJDBZQHDbQJZDBZQKBDKZBDLBQDZLIdBQUDb");
+            userCookie.setMaxAge(60 * 60 * 24); // 1 jour
+            response.addCookie(userCookie);
+            request.setAttribute("message", "Connexion réussie ! Bienvenue, " + username + ".");
+        } else {
+            request.setAttribute("message", "Nom d'utilisateur ou mot de passe incorrect.");
+        }
+        return "loginResult";
+    }
+}
---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,62 @@
+package com.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private int age;
+    private String email;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
