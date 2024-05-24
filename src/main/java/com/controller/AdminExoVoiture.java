163c18dcad19b2688d49c5de01d2c6271872ae7a
Tom DEPREZ <tom_deprez@hotmail.fr>
2024-05-24 11:59:06 +0200

    Ajouter le form exo créer une voiture

--- src/main/java/com/controller/AdminExoVoiture.java
+++ src/main/java/com/controller/AdminExoVoiture.java
@@ -3,6 +3,8 @@ package com.controller;
 import java.beans.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 import javax.sql.DataSource;
 
@@ -11,8 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.entity.Voiture;
+import com.repository.VoitureRepository;
 
 @Controller
 @RequestMapping("/admin/exo/voiture")
@@ -20,11 +24,33 @@ public class AdminExoVoiture {
 
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private VoitureRepository voitureRepository;
 
     @GetMapping("/afficher")
     public String afficherLesVoitures(Model model) {
+        List<Voiture> voitures = voitureRepository.findAll();
 
+        model.addAttribute("voitures", voitures);
         return "admin/exoVoiture/index";
     }
 
+    @GetMapping("/supprimer")
+    public String supprimerUneVoiture(@RequestParam Long id) {
+        voitureRepository.deleteById(id);
+        return "redirect:/admin/exo/voiture/afficher";
+    }
+
+    @GetMapping("/ajouter/modifier/afficher")
+    public String ajouterUneVoiture(Model model, @RequestParam(defaultValue = "0") Long id) {
+
+
+        return "admin/exoVoiture/form";
+    }
+
+    @GetMapping("/ajouter/modifier")
+    public String ajouterUneVoiture(@RequestParam(defaultValue = "0") Long id) {
+        
+        return "admin/exoVoiture/form";
+    }
 }
